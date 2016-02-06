d541c47ecd39365e8e276ccf467695e669868f65
barrymcgee <barry.mcgee@integralcode.co.uk>
2016-02-06 18:47:06 +0000

    Add '.md' files to jekyll-rebuild watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch(['_sass/**/*.scss'], ['sass']);
-    gulp.watch(['**/*.html', '**/*.markdown'], ['jekyll-rebuild']);
+    gulp.watch(['**/*.html', '**/*.markdown', '**/*.md'], ['jekyll-rebuild']);
 });
 
 /**
