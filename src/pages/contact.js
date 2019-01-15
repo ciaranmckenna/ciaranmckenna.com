7c50bc308e5d12657ae27156a38d6280dc47c5e8
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-01-15 23:09:21 +0000

    Add success page to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => (
     <SEO title="Contact" />
     <h1>Hello.</h1>
     <p>Send me a message using the form below and I'll get back to you asap.</p>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" action="/success">
       <p>
         <label>
           Your name: <input type="text" name="name" required />
---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Success = () => (
+  <Layout>
+    <SEO title="Contact - Success!" />
+    <h1>Success! 👌🏻</h1>
+    <p>Thank you for the message - I'll be in touch soon.</p>
+  </Layout>
+);
+
+export default Success;
