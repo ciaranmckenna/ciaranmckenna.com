959048882e923a4e2f8d1abc5e6dc89ccc41566b
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-01-20 13:26:08 +0000

    Use ajax contact form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,45 +1,89 @@
 import React from 'react';
+import { navigate } from 'gatsby-link';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Contact = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <h1>Hello.</h1>
-    <p>Send me a message using the form below and I'll get back to you asap.</p>
-    <form
-      name="Contact"
-      method="POST"
-      data-netlify="true"
-      action="/success/"
-      netlify-honeypot="bot-field"
-    >
-      <p>
-        <label>
-          Your name: <input type="text" name="name" required />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your email: <input type="email" name="email" required />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your message: <textarea name="message" required />
-        </label>
-      </p>
-      <p style={{ display: 'none' }}>
-        <label>
-          Don’t fill this out if you're human: <input name="bot-field" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send message</button>
-      </p>
-    </form>
-  </Layout>
-);
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
 
-export default Contact;
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error));
+  };
+  render() {
+    return (
+      <Layout>
+        <SEO title="Contact" />
+        <h1>Hello.</h1>
+        <p>
+          Send me a message using the form below and I'll get back to you asap.
+        </p>
+        <form
+          name="Contact"
+          method="post"
+          action="/success/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{' '}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send message</button>
+          </p>
+        </form>
+      </Layout>
+    );
+  }
+}
