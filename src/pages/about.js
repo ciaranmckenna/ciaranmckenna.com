8d367898e37c5d330f15fe9418f6780d85423821
Ciaran McKenna <32311254+ciaranmckenna@users.noreply.github.com>
2019-02-03 21:12:30 +0000

    Fixed typo on src/pages/about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = ({ location }) => (
     <h1>About me</h1>
     <p>
       I am a recent software development graduate, beginning my career as a back end developer. I was previously
-      a teacher and head of department before joinging the world of software development.
+      a teacher and head of department before joining the world of software development.
       I regularly invest in my own knowledge portfolio to enhance and cultivate my craft. 
     </p>
     <p>
