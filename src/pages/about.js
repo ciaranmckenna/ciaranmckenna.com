25cd626d3cba3b77ed48f08480e609faca4217c3
ciaranmckenna <hello@ciaranmckenna.com>
2021-11-16 21:47:49 +0000

    Remove 'recent' in 'about me' section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,12 +7,9 @@ const About = ({ location }) => (
     <SEO title="About me" />
     <h1>About me</h1>
     <p>
-      I am a recent software development graduate, beginning my career as a back end developer. I was previously
+      I am a development graduate, beginning my career as a back end developer. I was previously
       a teacher and head of department before joining the world of software development.
       I regularly invest in my own knowledge portfolio to enhance and cultivate my craft. 
-    </p>
-    <p>
-      I believe quality is always a requirements issue.  
     </p>
   </Layout>
 );
