60f78de87f206a30716decf8909feb7fd07619e6
ciaranmckenna <hello@ciaranmckenna.com>
2021-11-16 21:53:18 +0000

    Incl SE in about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const About = ({ location }) => (
     <SEO title="About me" />
     <h1>About me</h1>
     <p>
-      I am a development graduate, beginning my career as a back end developer. I was previously
+      I am a software engineer, beginning my career as a back end developer. I was previously
       a teacher and head of department before joining the world of software development.
       I regularly invest in my own knowledge portfolio to enhance and cultivate my craft. 
     </p>
